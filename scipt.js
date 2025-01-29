0fd171f59f96ba50f011a1e0b9202324b1f9965c
mcher0 <mansuperman.ms@gmail.com>
2025-01-29 13:25:21 +0100

    added the ability to remove reset the grid

--- scipt.js
+++ scipt.js
@@ -1,19 +1,44 @@
 const container = document.querySelector("#container");
+const newGridButton = document.querySelector("#newGrid");
 const gridCell = document.createElement("div");
 let planeDimensions =   16;
-const gridCellWidth = container.clientWidth/planeDimensions - 2;
-const gridCellHeight = container.clientHeight/planeDimensions -2;
-for(let i = 0; i < planeDimensions*planeDimensions;i++){
-    const gridCell = document.createElement("div");
-    gridCell.setAttribute("class","gridCell");
-    gridCell.setAttribute("style",`height:${gridCellHeight}px; width:${gridCellWidth}px; border: 1px solid black;`);
-    container.appendChild(gridCell);
+
+function createGrid (){
+    const gridCellWidth = container.clientWidth/planeDimensions - 2;
+    const gridCellHeight = container.clientHeight/planeDimensions -2;
+    for(let i = 0; i < planeDimensions*planeDimensions;i++){
+        const gridCell = document.createElement("div");
+        gridCell.setAttribute("class","gridCell");
+        gridCell.setAttribute("style",`height:${gridCellHeight}px; width:${gridCellWidth}px; border: 0.5px solid black;`);
+        container.appendChild(gridCell);
+    }
+    container.addEventListener("mouseover",(event) => {
+        let target = event.target;
+        console.log(target.className);
+        console.log("smth happened");
+        if (target.className === "gridCell"){
+            target.style.backgroundColor = 'red';
+        }
+    })
 }
-container.addEventListener("mouseover",(event) => {
-    let target = event.target;
-    console.log(target.className);
-    console.log("smth happened");
-    if (target.className === "gridCell"){
-        target.style.backgroundColor = 'red';
+createGrid();
+
+function removeGrid(){
+    const gridCells = document.querySelectorAll(".gridCell");
+    console.log(gridCells);
+    for(let cell =0; cell < gridCells.length;cell++){
+        gridCells[cell].remove();
+    }
+}
+
+
+newGridButton.addEventListener("click",() =>{
+  newGridDimensions = prompt("Please enter new grid dimensions(1-100)","16")
+    if(newGridDimensions<=100 && newGridDimensions>0){
+        planeDimensions = newGridDimensions;
+        removeGrid();
+        createGrid();
+    }else{
+        alert("hell nah bruh");
     }
 })
